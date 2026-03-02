1975d713ba7ecfba7dc8106738ed102b6e4dbeda
Fily16 <yanfilybacatorres@gmail.com>
2026-03-02 17:08:35 -0500

    nose

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tu-dominio.com/api',
+  apiUrl: 'https://servermanager-backend.onrender.com/api',
+  whatsappServiceUrl: 'https://whatsapp-baileys-bot.onrender.com/api',
   defaultBusinessId: 1
 };
